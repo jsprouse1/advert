56ee9d6ab7204175b196044ee64440cc80656ff6
tacksoo <tim@ggc.edu>
2016-03-13 05:51:38 +0000

    init

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'uri'
+require 'active_record'
+
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///mycustomers')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
+
+class Customer < ActiveRecord::Base
+end
+
